f40ed9a217aa6c64919a664213c5b6c21c84acc8
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-06-07 11:50:49 +0530

    Added solution - LeetHub

--- 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
+++ 0 - 1 Knapsack Problem - GFG/0-1-knapsack-problem.cpp
@@ -8,6 +8,7 @@ class Solution
 {
     public:
     //Function to return max value that can be put in knapsack of capacity W.
+    /*
     int help( int idx, int W, int wt[], int val[], vector<vector<int>> &dp )
     {
         //base case
@@ -32,6 +33,27 @@ class Solution
         vector<vector<int>> dp( n, vector<int> ( W + 1, -1 ) );
        return help( n-1, W, wt, val, dp );
     }
+    */
+    int knapSack(int W, int wt[], int val[], int n) 
+    { 
+       vector<vector<int>> dp( n, vector<int> ( W + 1, 0 ) );
+       for( int i = wt[0]; i <= W; ++i ) dp[0][i] = val[0];
+       
+       for( int i = 1; i < n; ++i )
+       {
+           for( int j = 0; j <= W; ++j  )
+           {
+               int notTake = dp[i-1][j];
+               int take = INT_MIN;
+               if( wt[i] <= j )
+               {
+                   take = val[i] + dp[i-1][j-wt[i]];
+               }
+               dp[i][j] = max( take,notTake);
+           }
+       }
+       return dp[n-1][W];
+     }
 };
 
 //{ Driver Code Starts.
