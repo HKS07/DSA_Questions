d6323dbb0fd48ed0bea40bd44a44c4f7064c11c0
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-06-17 11:47:21 +0530

    Time: 27 ms (38.64%), Space: 46.4 MB (49.28%) - LeetHub

--- 2717-semi-ordered-permutation/2717-semi-ordered-permutation.cpp
+++ 2717-semi-ordered-permutation/2717-semi-ordered-permutation.cpp
@@ -4,27 +4,10 @@ public:
         
         int onePos = -1, nPos = -1,size = nums.size();
 
-        // //left
-        // for( int i = 0; i < size; ++i )
-        // {
-        //     if( nums[i] == 1 )
-        //     {
-        //         onePos = i; break;
-        //     }
-        // }
-        // //right 
-        // for( int i = size-1; i >= 0; --i )
-        // {
-        //     if( nums[i] == size )
-        //     {
-        //         nPos = i; break;
-        //     }
-        // }
-        
-        for( int i = 0, j = size - 1; j >=0; ++i, --j )
+        for( int i = 0; i < size; ++i )
         {
-            if( nums[i] == 1 && onePos == -1 ) onePos = i;
-            if( nums[j] == size && nPos == -1 ) nPos = j;
+            if( nums[i] == 1 ) onePos = i;
+            if( nums[i] == size ) nPos = i;
         }
 
         if( onePos < nPos ) return onePos + size - nPos - 1;
