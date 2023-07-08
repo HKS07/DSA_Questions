ef7a154c54126158e9052d82e17936e3e30f56ca
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-07-09 03:17:50 +0530

    Time: 55 ms (24.01%), Space: 43.6 MB (17.55%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    void help( int idx, vector<int>& c, vector<vector<int>>& s, vector<int> t, int targ )
+    vector<vector<int>> sol;
+    void help( int idx, int targ, vector<int>& c, vector<int> temp)
     {
         if( idx == c.size() )
         {
-            if( targ == 0 ) s.push_back(t);
+            if( targ == 0 ) sol.push_back( temp );
             return;
         }
-        if( targ == 0 ) {s.push_back(t);
-            return;}
-        if( targ < 0 )  return;
-        //take value at idx
-        t.push_back(c[idx]);
-        help(idx,c,s,t,targ-c[idx]);
-        t.pop_back();
-        help(idx+1,c,s,t,targ);
+        
+        if( c[idx] <= targ )
+        {
+            temp.push_back( c[idx] );
+            help( idx, targ - c[idx], c, temp );
+            temp.pop_back();
+        }
+        help( idx + 1, targ, c, temp );
     }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int>> t;
-        help( 0, candidates, t, {}, target);
-        return t;
+        help(0,target,candidates,{});
+        return sol;
     }
 };
