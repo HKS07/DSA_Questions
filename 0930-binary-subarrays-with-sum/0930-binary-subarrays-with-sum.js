7eeb25f1d95b330a110a82a88838527296e67d41
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-07-17 03:25:40 +0530

    Time: 5893 ms (5.49%), Space: 44.4 MB (65.24%) - LeetHub

---
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} goal
+ * @return {number}
+ */
+const numSubarraysWithSum = (nums, goal) => {
+    let i = 0, answer = 0
+    while(i < nums.length){
+        let j = i, sum = 0
+        while(j < nums.length){
+            if(nums[j] == 1) sum++
+            if(sum == goal) answer++
+            if(sum > goal) break
+            j++
+        }
+        i++
+    }
+    return answer
+}
