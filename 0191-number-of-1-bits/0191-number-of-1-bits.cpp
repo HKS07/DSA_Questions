cbab62c36ebf8a48c31dc8848b8ff8a226d18840
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-07-17 13:24:02 +0530

    Time: 5 ms (30.17%), Space: 6.1 MB (23.70%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.cpp
+++ 0191-number-of-1-bits/0191-number-of-1-bits.cpp
@@ -1,11 +1,20 @@
 class Solution {
 public:
+    // int hammingWeight(uint32_t n) {
+    //     int sol = 0;
+    //     while(n>0)
+    //     {
+    //         if( n & 1 ) sol++;
+    //         n >>= 1;
+    //     }
+    //     return sol;
+    // }
     int hammingWeight(uint32_t n) {
         int sol = 0;
         while(n>0)
         {
-            if( n & 1 ) sol++;
-            n >>= 1;
+            n = n & n-1;
+            sol++;
         }
         return sol;
     }
