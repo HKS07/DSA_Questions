54e10fcd9aaa5c05a4cb73b55dd998794430b1a3
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-07-17 13:30:30 +0530

    Time: 26 ms (44.12%), Space: 23.2 MB (58.68%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    void reverseString(vector<char>& s) {
+        
+        for( int i = 0, j = s.size()-1; i < s.size()/2; ++i, --j )
+        {
+            s[i] = s[i]^s[j];
+            s[j] = s[j]^s[i];
+            s[i] = s[i]^s[j];
+        }
+    }
+};
