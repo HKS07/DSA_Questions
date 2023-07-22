dd04aba7ba02ae46cffec32ce848b8e6378e6658
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-07-22 22:53:33 +0530

    Time: 82 ms (48.27%), Space: 24.5 MB (25.21%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-    /*int helper( string s1, string s2, int i, int j, vector<vector<int>> &dp )
+    int help( string &s1, string &s2, int i, int j, vector<vector<int>> &dp )
     {
-        //base case
         if( i < 0 || j < 0 ) return 0;
-        //memoization 
-        if( dp[i][j] != -1 ) return dp[i][j]; 
-        // condition
-        if( s1[i] == s2[j] )
-            return dp[i][j] = 1 + helper( s1, s2, i - 1, j - 1, dp );
+        if( dp[i][j] != -1 ) return dp[i][j];
+        
+        if( s1[i] == s2[j] ) return dp[i][j] = 1 + help(s1,s2, i-1,j-1,dp);
         else
-            return dp[i][j] = max( helper( s1, s2, i - 1, j, dp ), helper( s1, s2, i, j - 1, dp ));
+        {
+            return dp[i][j] = 0 + max(help( s1, s2, i-1,j,dp),help( s1, s2, i, j - 1,dp)) ;
+        }
+        
+        
     }
     int longestCommonSubsequence(string text1, string text2) {
-        vector<vector<int>> dp( text1.length(), vector<int>(text2.length(), -1));
-        
-        return helper( text1, text2, text1.length() - 1, text2.length() - 1, dp );
-    }*/
-    int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.length(), m = text2.length();
-        vector<vector<int>> dp( n + 1, vector<int> (m + 1, -1 ));
-        for( int i = 0; i <= n; ++i ) dp[i][0] = 0;
-        for( int i = 0; i <= m; ++i ) dp[0][i] = 0;
-        
-        for( int i = 1; i <= n; ++i )
-        {
-            for( int j = 1; j <=m; ++j )
-            {
-                if( text1[i -1 ] == text2[j -1 ] ) dp[i][j] = dp[i-1][j-1] + 1;
-                else dp[i][j] = max( dp[i-1][j], dp[i][j-1]);
-            }
-        }
-        return dp[n][m];
+        int l1 = text1.length(), l2 = text2.length();
+        vector<vector<int>> dp(l1, vector<int> (l2 , -1));
+        return help(text1,text2, l1-1, l2-1, dp);
     }
 };
