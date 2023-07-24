a7b1fb8e1afba1f1f0786314cbde31b1575be839
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-07-24 14:18:57 +0530

    Time: 8 ms (58.70%), Space: 13.3 MB (35.58%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,54 +1,40 @@
 class Solution {
 public:
     int orangesRotting(vector<vector<int>>& grid) {
-        int row = grid.size(), col = grid[0].size();
+        int n = grid.size(), m = grid[0].size();
         queue<pair<int,int>> q;
         int fresh = 0, time = 0;
         
-        for( int i = 0; i < row; ++i )
+        for( int i = 0; i < n; ++i )
         {
-            for( int j= 0; j < col; ++j  )
+            for( int j= 0; j < m; ++j  )
             {
                 if(grid[i][j] == 1 ) fresh++;
                 if( grid[i][j] == 2) q.push({i,j});
             }
         }
-        vector<vector<int>> vis(row, vector<int> (col,-1));
+        vector<int> delR = { 0,-1,0,1};
+        vector<int> delC = { -1,0,1,0};
+        vector<vector<int>> vis(n, vector<int> (m,0));
         while( !q.empty() && fresh > 0 )
         {
             int size = q.size();
             time++;
             for( int i = 0; i < size; ++i )
             {
-                int r = q.front().first, c = q.front().second; q.pop();
+                int row = q.front().first, col = q.front().second; q.pop();
                 
-                //up
-                if( r - 1 >= 0 && grid[r-1][c] == 1 && vis[r-1][c] == -1 )
+                for( int del = 0; del < 4; ++del )
                 {
-                    vis[r-1][c] = 1;
-                    q.push({r-1,c});
-                    fresh--;
-                }
-                //down
-                if( r + 1 < row && grid[r + 1][c] == 1 && vis[r + 1][c] == -1 )
-                {
-                    vis[r + 1][c] = 1;
-                    q.push({r + 1,c});
-                    fresh--;
-                }
-                //left
-                if( c - 1 >= 0 && grid[r][c-1] == 1 && vis[r][c-1] == -1 )
-                {
-                    vis[r][c-1] = 1;
-                    q.push({r,c-1});
-                    fresh--;
-                }
-                //right
-                if( c + 1 < col && grid[r][c+1] == 1 && vis[r][c+1] == -1 )
-                {
-                    vis[r][c+1] = 1;
-                    q.push({r,c+1});
-                    fresh--;
+                    int r = row + delR[del], c = col + delC[del];
+                    if( r >= 0 && r < n && c >= 0 && c < m && grid[r][c] == 1 && !vis[r][c] )
+                    {
+                        vis[r][c] = 1;
+                        q.push({r,c});
+                        grid[r][c] = 2;
+                        fresh--;
+                        
+                    }
                 }
             }
         }
