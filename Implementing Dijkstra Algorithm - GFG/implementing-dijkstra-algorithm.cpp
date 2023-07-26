99ce778a50485623ef152152046547edab8cfa26
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-07-26 15:32:01 +0530

    Added solution - LeetHub

--- Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
+++ Implementing Dijkstra Algorithm - GFG/implementing-dijkstra-algorithm.cpp
@@ -8,34 +8,31 @@ class Solution
 	public:
 	//Function to find the shortest distance of all the vertices
     //from the source vertex S.
-    vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+    vector <int> dijkstra(int v, vector<vector<int>> adj[], int s)
     {
-         priority_queue< pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq; 
-         vector<int> dist(V);
-         
-         for( int i = 0; i < V; ++i ) dist[i] = 1e9;
-         dist[S] = 0;
-         pq.push({0,S});
-         
-         while( !pq.empty() )
-         {
-             int node = pq.top().second;
-             int dis = pq.top().first;
-             pq.pop();
-             
-             for( auto it: adj[node] )
-             {
-                 int edgeWt = it[1];
-                 int adjNode = it[0];
-                 
-                 if( dis + edgeWt < dist[adjNode] )
-                  {
-                      dist[adjNode] = dis + edgeWt;
-                      pq.push({ dist[adjNode], adjNode});
-                  }
-             }
-         }
-         return dist;
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>> > pq;
+        vector<int> dist(v,1e9);
+        
+        dist[s] = 0;
+        pq.push({0,s});// {dist, source}
+        
+        while( !pq.empty() )
+        {
+            int dis = pq.top().first, node = pq.top().second;
+            pq.pop();
+            
+            for( auto n: adj[node] )
+            {
+                int adjNode = n[0], wt = n[1];
+                int d = dis + wt;
+                if( d < dist[adjNode] )
+                {
+                    pq.push({d,adjNode});
+                    dist[adjNode] = d;
+                }
+            }
+        }
+        return dist;
     }
 };
 
