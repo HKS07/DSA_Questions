9c3fa3ef3c5a4d2ef88f72df8ef7704c60b3ac85
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-08-05 20:18:22 +0530

    Added solution - LeetHub

---
+++ Shortest Path in Weighted undirected graph - GFG/shortest-path-in-weighted-undirected-graph.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> shortestPath(int n, int m, vector<vector<int>>& edges) {
+        vector<int> dist(n+1,1e9),parent(n+1),sol;
+        dist[1] = 0;
+        vector<pair<int,int>> adj[n+1];
+        for( auto i: edges )
+        {
+            int u = i[0], v = i[1], wt = i[2];
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
+        }
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> q;// {distance, source}
+        q.push({0,1});
+        
+        for( int i = 1; i <= n; ++i ) parent[i] = i;
+        
+        while( !q.empty() )
+        {
+            int node = q.top().second, dis = q.top().first;
+            q.pop();
+            for( auto it: adj[node] )
+            {
+                int adjNode = it.first, wt = it.second;
+                if( dis + wt < dist[adjNode] )
+                {
+                    dist[adjNode] = dis + wt;
+                    q.push({dist[adjNode] , adjNode});
+                    parent[adjNode] = node;
+                }
+            }
+        }
+        if( dist[n] == 1e9 ) return {-1};
+        int node = n;
+        while( node != parent[node] )
+        {
+            sol.push_back(node);
+            node = parent[node];
+        }
+        sol.push_back(1);
+        reverse( begin(sol),end(sol) );
+        return sol;
+
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        vector<vector<int>> edges;
+        for (int i = 0; i < m; ++i) {
+            vector<int> temp;
+            for (int j = 0; j < 3; ++j) {
+                int x;
+                cin >> x;
+                temp.push_back(x);
+            }
+            edges.push_back(temp);
+        }
+
+        Solution obj;
+        vector<int> res = obj.shortestPath(n, m, edges);
+        for (auto x : res) {
+            cout << x << " ";
+        }
+        cout << "\n";
+    }
+}
+
+// } Driver Code Ends
