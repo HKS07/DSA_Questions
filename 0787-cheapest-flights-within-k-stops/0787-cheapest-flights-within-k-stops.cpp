263eb4e6833ebe6c9c4cf6e6324174064867e5f7
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-08-11 15:47:37 +0530

    Time: 30 ms (38.26%), Space: 15.6 MB (12.20%) - LeetHub

--- 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
+++ 0787-cheapest-flights-within-k-stops/0787-cheapest-flights-within-k-stops.cpp
@@ -1,34 +1,41 @@
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int k) {
-        // vector<pair<int,int>> adj[n];
-        vector<vector<pair<int, int>>> adj(n);
-        for(auto flight : flights){
-            // flight[0] represent node i, flight[1] represent neighbor node of node i, flight[2] represent cost between node i to neighbor node
-            adj[flight[0]].push_back({flight[1], flight[2]});
+        vector<vector<pair<int, int>>> graph(n);
+        for(auto e : flights) {
+            graph[e[0]].push_back({e[1], e[2]});
         }
-        //it will store [node, cost]
-        queue<pair<int, int>> q;
-        q.push({src, 0});
-        //it will store minimum cost to reach each node
-        vector<int> minCost(n, INT_MAX);
-        int stops = 0;
-        while(!q.empty() && stops <= k){
-            int size = q.size();
-            while (size--) {
-                auto [currNode, cost] = q.front();
-                q.pop();
-                for (auto& [neighbour, price] : adj[currNode]) {
-                    if (price + cost < minCost[neighbour]){
-                        minCost[neighbour] = price + cost;
-                        q.push({neighbour, minCost[neighbour]});
-                    }
-                }
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>> pq;
+        // {dist_from_src_node, node, number_of_stops_from_src_node}
+        pq.push({0, src, 0});
+        
+        vector<int> stops(n, INT_MAX);
+        // number of stops to reach indexth node with least possible price from src node
+        // as it will be calculated once the pq.top() equals to indexth node
+        
+        // pq.top() will always store least cost among the pq elements so if already stop is calculated
+        // and if that is greater than the cstop that means already we have a path with cheaper cost
+        // as well as with less or equal number of stops
+        
+        while(!pq.empty()) {
+            auto temp=pq.top();
+            int cdist=temp[0];
+            int cnode=temp[1];
+            int cstop=temp[2];
+            pq.pop();
+            
+            if(cstop>stops[cnode] || cstop>k+1)
+                continue;
+            
+            stops[cnode]=cstop;
+            if(cnode==dst) {
+                return cdist;
             }
-            stops++;
+            
+            for(auto a : graph[cnode]) {
+                pq.push({cdist+a.second, a.first, cstop+1});
+            }
         }
-        if(minCost[dst] == INT_MAX)
-            return -1;
-        return minCost[dst];
+        return -1;
     }
 };
