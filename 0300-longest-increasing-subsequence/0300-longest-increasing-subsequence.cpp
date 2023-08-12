26475ffee5c31060e534f04381a97f92eac3b821
HKS07 <77383317+HKS07@users.noreply.github.com>
2023-08-12 15:07:23 +0530

    Time: 487 ms (42.82%), Space: 292.1 MB (27.05%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -13,9 +13,27 @@ public:
             
         return dp[idx][prevIdx+1] = len;
     }
-    int lengthOfLIS(vector<int>& nums) {
-        vector<vector<int> > dp( nums.size(), vector<int>( nums.size() + 1, -1 ));
+    int lengthOfLIS(vector<int>& arr) {
+//         vector<vector<int> > dp( nums.size(), vector<int>( nums.size() + 1, -1 ));
         
-        return lenHelp( 0, -1, nums, nums.size(), dp );
+//         return lenHelp( 0, -1, nums, nums.size(), dp );
+        int n = arr.size();
+        vector<vector<int>> dp(n+1, vector<int> (n+1,0));
+
+        for( int idx = n-1; idx >= 0; --idx )
+        {
+            for( int preIdx = idx-1; preIdx >=-1 ; --preIdx )
+            {
+                int len = 0 + dp[idx+1][preIdx+1];
+
+                if( preIdx == -1 || arr[idx] > arr[preIdx] )
+                {
+                    len = max( len , 1 + dp[idx+1][idx+1]);
+                }
+                dp[idx][preIdx+1] = len;
+            }
+        }
+        return dp[0][0];
+        
     }
 };
